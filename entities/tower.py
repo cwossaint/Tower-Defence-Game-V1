aac544441d60d9860b045c99f4e6e0dcc19389f9
cwossaint <chrishjlaw@gmail.com>
2025-03-13 15:00:15 +0800

    added tower render method

--- entities/tower.py
+++ entities/tower.py
@@ -1,13 +1,24 @@
 from tower_sprites import *
 
 class Tower():
+
+    all_towers = []
+
     def __init__(self) -> None:
-        
+        self.sprite = None
+        self.range = None
+        self.dmg = None
+        self.cost = None
+        self.attack_spd = None
+        self.x = None
+        self.y = None
+        self.all_towers.append(self)
+
     def target_enemy():
         pass
 
-    def render():
-        pass
+    def render(self, screen):
+        screen.blit(self.sprite (self.x, self.y))
 
     def upgrade():
         pass
